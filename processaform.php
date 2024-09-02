2e99d1b7bb74712ecb33af29a86fe4b8059fc5f0
RafaelKdl <rafaelkudlaviec@gmail.com>
2024-09-02 14:41:21 -0300

    Add files via upload

---
+++ processaform.php
@@ -0,0 +1,26 @@
+<?php
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $nome = $_POST['camponome'];
+    $email = $_POST['campoemail'];
+    $telefone = $_POST['campotelefone'];
+    $assunto = $_POST['assunto'];
+    $mensagem = $_POST['mensagem'];
+
+
+    $destinatario="rafaelkudlaviec@gmail.com";  
+    $fonte="Mensagem de contatos";
+
+    $corpo = "Nome do contato: $nome \n";
+    $corpo .= "E-mail do contato: $email \n";
+    $corpo .= "Telefone do contato: $telefone \n";
+    $corpo .= "Assunto: $assunto \n";
+    $corpo .= "Mensagem: $mensagem \n";
+   
+
+    mail($destinatario, $assunto, $corpo);
+
+    header("Location:obrigado.html");
+    exit();
+}
+
+?>
